d23f5f9fe5964ebd0deafbfb87ffe76ea8d18cd5
developersbiuro <developersbiuro@gmail.com>
2023-08-31 14:27:26 +0500

    next js

--- app/services/page.js
+++ app/services/page.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from '../src/component/header/Header'
 import Footer from '../src/component/footer/Footer'
+import ServiceCard from '../src/component/servicecard/ServiceCard'
+import webimg from "../src/assets/images/pngwing.com (4).png"
+import desimg from "../src/assets/images/pngwing.com (3).png"
+import marimg from "../src/assets/images/pngwing.com (7).png"
+
+
 
 export default function page() {
   return (
     <div>
       <Header/>
 
+      <ServiceCard src={webimg} dg="jhdfjkfhjk" title="Web Development" />
+      <ServiceCard src={desimg} dg="jhdfjkfhjk" title="Graphic Design" />
+      <ServiceCard src={marimg} dg="jhdfjkfhjk" title="Digital Marketing" />
       <Footer/>
     </div>
   )
---
+++ app/src/component/servicecard/ServiceCard.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Props } from 'next/script'
+
+
+export default function ServiceCard(props) {
+    return (
+        <div className='col-lg-4 col-sm-10 '>
+            <div className="card acd" >
+                <Image src={props.src} className="card-img-top bacimg"  ></Image>
+                <div className="card-body wbd">
+                    <h3>{props.title}</h3>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmodtempor
+                        incididunt ut labore et dolore</p>
+                </div>
+            </div>
+        </div>
+    )
+}
